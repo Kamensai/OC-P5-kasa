5fc3f559d16bac172beb65b389907dffe1229568
Saïsana <khamvongsa.v@hotmail.fr>
2025-07-11 12:59:45 +0200

    Page à propos terminée. Avec le collapse mis en place et l'affichage du texte au clique sur l'icône

---
+++ src/components/Collapse/index.jsx
@@ -0,0 +1,31 @@
+import { useState } from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faChevronUp } from '@fortawesome/free-solid-svg-icons'
+
+function Collapse({ collapseTitle, collapseContent }) {
+  const [isOpen, setIsOpen] = useState(false)
+
+  return (
+    <div className="collapse">
+      <div className="collapse-title">
+        <div className="collapse-title-text">{collapseTitle}</div>
+        <div className="collapse-toggle">
+          <FontAwesomeIcon
+            icon={faChevronUp}
+            size="xl"
+            onClick={() => setIsOpen(!isOpen)}
+            style={{
+              transform: isOpen ? 'rotate(-180deg)' : 'rotate(0deg)',
+              transition: 'transform 0.3s',
+            }}
+          />
+        </div>
+      </div>
+      <div className={`collapse-content ${isOpen ? 'open' : ''}`}>
+        {collapseContent}
+      </div>
+    </div>
+  )
+}
+
+export default Collapse
--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -1,8 +1,37 @@
+import ImageBanner from '../../components/ImageBanner'
+
+import imagePropos from '../../assets/image_propos.png'
+
+import Collapse from '../../components/Collapse'
+
 function About() {
   return (
-    <div>
-      <h1>A propos</h1>
-    </div>
+    <section className="apropos">
+      <ImageBanner
+        imgBannerSrc={imagePropos}
+        imgBannerAlt="Image de montagnes"
+      />
+
+      <Collapse
+        collapseTitle="Fiabilité"
+        collapseContent="Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées  par nos équipes."
+      />
+
+      <Collapse
+        collapseTitle="Respect"
+        collapseContent="La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme."
+      />
+
+      <Collapse
+        collapseTitle="Service"
+        collapseContent="La qualité du service est au cœur de notre engagement chez Kasa. Nous veillons à ce que chaque interaction, que ce soit avec nos hôtes ou nos locataires, soit empreinte de respect et de bienveillance."
+      />
+
+      <Collapse
+        collapseTitle="Sécurité"
+        collapseContent="La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes."
+      />
+    </section>
   )
 }
 
